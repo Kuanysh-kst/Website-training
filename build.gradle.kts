cff1fb39214b46670a098e0d907fa27717211299
Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>
2024-12-23 16:41:52 +0500

    added new implementation for jwt token
    Signed-off-by: Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,12 +25,15 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	//implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("net.datafaker:datafaker:2.4.2")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+	implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
