a967e71cdcc79fa3d1217bdb7ddb471199fb815d
Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>
2025-02-09 22:53:25 +0500

    repeated test annotation
    Signed-off-by: Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>

--- src/test/java/com/example/junit/DemoRepeatedTest.java
+++ src/test/java/com/example/junit/DemoRepeatedTest.java
@@ -1,4 +1,42 @@
 package com.example.junit;
 
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class DemoRepeatedTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void beforeEachTestMethod() {
+        calculator = new Calculator();
+        System.out.println("Executing @BeforeEach method!");
+    }
+
+    @DisplayName("Division by zero")
+    @RepeatedTest(value = 3, name = "{displayName}. Repetition {currentRepetition} of {totalRepetitions}")
+    void testIntegerDivision_WhenDividendDividedByZero_ShouldThrowArithmeticException(
+            RepetitionInfo repetitionInfo, TestInfo testInfo) {
+        if (testInfo.getTestMethod().isPresent()) {
+            System.out.printf("Running method: %s%n", testInfo.getTestMethod().get().getName());
+        }
+
+        System.out.printf("Division by zero! № %d of %d%n",
+                repetitionInfo.getCurrentRepetition(), repetitionInfo.getTotalRepetitions());
+        var dividend = 3;
+        var division = 0;
+        String expectedExceptionMessage = "/ by zero";
+
+        // Act & Assert
+        ArithmeticException actualException = assertThrows(ArithmeticException.class, () -> {
+            // Act
+            calculator.integerDivision(dividend, division);
+        }, "Division by zero should have thrown Arithmetic exception");
+
+        // Assert
+        assertEquals(expectedExceptionMessage, actualException.getMessage(), () ->
+                String.format("Expected %s, but was %s", expectedExceptionMessage, actualException.getMessage())
+        );
+    }
 }
