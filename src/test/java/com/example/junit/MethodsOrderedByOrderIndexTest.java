c1536cc584db116168fea24f5b4fc40abd57d492
Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>
2025-02-11 20:59:45 +0500

    methods ordering by order annotation index
    Signed-off-by: Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>

---
+++ src/test/java/com/example/junit/MethodsOrderedByOrderIndexTest.java
@@ -0,0 +1,33 @@
+package com.example.junit;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodsOrderedByOrderIndexTest {
+    @Order(1)
+    @Test
+    void testC() {
+        System.out.println("Running test C");
+    }
+
+    @Order(2)
+    @Test
+    void testB() {
+        System.out.println("Running test B");
+    }
+
+    @Order(3)
+    @Test
+    void testD() {
+        System.out.println("Running test D");
+    }
+
+    @Order(4)
+    @Test
+    void testA() {
+        System.out.println("Running test A");
+    }
+}
