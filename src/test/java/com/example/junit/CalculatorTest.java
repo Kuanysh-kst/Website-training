7b0ef79b2659bcb67c6648389a1a0791b311c968
Kuanysh <kuanysh4646@gmail.com>
2025-02-26 10:39:58 +0500

    nit
    Signed-off-by: Kuanysh <kuanysh4646@gmail.com>

--- src/test/java/com/example/junit/CalculatorTest.java
+++ src/test/java/com/example/junit/CalculatorTest.java
@@ -1,6 +1,12 @@
 package com.example.junit;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
