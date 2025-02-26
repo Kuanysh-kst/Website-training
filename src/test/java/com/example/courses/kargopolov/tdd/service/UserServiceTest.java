4f38042cd960b239726360c47d25551f3f37eea3
Kuanysh <kuanysh4646@gmail.com>
2025-02-26 12:44:55 +0500

    creating user service
    Signed-off-by: Kuanysh <kuanysh4646@gmail.com>

---
+++ src/test/java/com/example/courses/kargopolov/tdd/service/UserServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.courses.kargopolov.tdd.service;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+    @Test
+    void testCreateUser_WhenUserDetailsProvided_returnsUserObject() {
+        // Arrange
+        UserService userService = new UserServiceImpl();
+
+        // Act
+
+        // Assert
+
+    }
+
+}
